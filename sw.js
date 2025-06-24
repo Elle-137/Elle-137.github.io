c50c04baef587f87ee086c1ef0b4af20d08a5926
Elle DR <m18561903257@163.com>
2025-06-24 17:00:21 +0800

    Site updated: 2025-06-24 17:00:21

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1750663408825";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1750755538182";const preCache = ["/images/taichi.png","/images/steamuserimages-amaihd.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
